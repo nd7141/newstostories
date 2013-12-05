d8d53bdf1733e910e0df556d2ec05b9c666b6085
Ivanov Sergey <ivanovserg990@gmail.com>
2013-12-05 22:40:54 +0400

    Add python file to sort time_stamps.txt by date

---
+++ data/sortByDate.py
@@ -0,0 +1,17 @@
+import operator
+import datetime
+
+with open('time_stamps.txt') as f:
+    docs = f.read().split('\n')
+
+documents = [datetime.datetime.strptime(doc, '%H:%M %d.%m.%Y').\
+             strftime('%Y-%m-%d %H-%M') for doc in docs]
+
+
+sorted_docs = [i for i in sorted(enumerate(documents), key=lambda x:x[1])]
+
+with open('sorted_time_stamps.txt','w') as f:
+    for doc in sorted_docs:
+        f.write(str(doc[0])+' '+doc[1]+'\n')
+
+        
