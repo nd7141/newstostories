c1b8ac3e0d98d33c90bbd5a71929c639f147b272
Ivanov Sergey <ivanovserg990@gmail.com>
2013-12-06 18:37:59 +0400

    Use array instead containers.Map

--- data/greedyAggregation.m
+++ data/greedyAggregation.m
@@ -1,21 +1,24 @@
-function doc2group = greedyAggregation(D,doc_ids,p)
+function [doc2group,g_num] = greedyAggregation(D,doc_ids,p)
 
 tic
 
 g_num = 1; % number of groups exist at the moment
-doc2group = containers.Map(doc_ids(1),g_num); % clustering news to groups
+% doc2group = containers.Map(doc_ids(1),g_num); % clustering news to groups
+doc2group = zeros(size(D, 1), 1);
+doc2group(1) = g_num;
 fprintf('Proceed %d document: ', 1);
 toc
 
 if nargin < 3
-    p = .8; % quntatile percentage for threshold
+    p = .2; % quntatile percentage for threshold
 end
-threshold = quantile(min(D,[],2),p);
+% threshold = quantile(min(D,[],2),p);
+threshold = mean(D(:)) * p;
 
 d = length(D); % number of documents D - square matrix of distances
 for i = 2:d
     idxs = doc_ids(1:i-1); % looking at already proceeded documents
-    [v,k] = min(D(i,idxs)); % finding the closest document among previous documents
+    [v,k] = min(D(doc_ids(i),idxs)); % finding the closest document among previous documents
     doc_id = doc_ids(i);
     if v <= threshold % if it is close enough
         prev_doc_id = doc_ids(k);
